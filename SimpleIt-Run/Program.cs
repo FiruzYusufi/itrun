51add3499e886ceacf86e75e78757b19b9b9d51c
Firuz <firikyusufi@gmail.com>
2022-12-08 06:00:49 -0800

    ..

---
+++ SimpleIt-Run/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+List<String> strings = new() {"abdujon", "1", "1", "1", "1", "1", "2","3","4", "4", "4", "4", "4", "4", "5","6","7","8"};
+
+
+var strs = strings.OrderByDescending(str=> str).Distinct().ToList();
+//var newstr = from s in strings where s == "1" select s;
+var newstr = strings.Where(s => s.ToUpper().StartsWith("A")).Distinct().ToList();
+string aa = "abdujon";
+string bb = aa.ToUpper();
+Console.WriteLine(bb);
+//foreach (var s in newstr)
+//{
+//    Console.WriteLine(s);
+//}
